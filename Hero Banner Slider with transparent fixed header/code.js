41f860bad5cad3977722b5a7ba225fac5e9ff8fb
Krey <developmentkrey@gmail.com>
2020-12-25 13:18:11 +0100

    Video Transparent fixed header completed

---
+++ Hero Banner Slider with transparent fixed header/code.js
@@ -0,0 +1,17 @@
+$(window).scroll(function(){
+    var scrollTop = window.scrollY;
+    console.log(scrollTop);
+    if(scrollTop>10){
+        document.querySelector('.headerContainer').style.backgroundColor = 'white';
+        document.querySelector('.headerContainer').style.boxShadow = '0px 0px 4px rgba(0,0,0,0.5)';
+        document.querySelector('.logo').style.filter = 'invert(1)';
+        document.querySelector('.menuHamburguer').style.filter = 'invert(1)';
+        document.querySelector('.menuContainer').style.filter = 'invert(1)';
+    }else{
+        document.querySelector('.headerContainer').style.backgroundColor = 'transparent';
+        document.querySelector('.headerContainer').style.boxShadow = 'none';
+        document.querySelector('.logo').style.filter = 'invert(0)';
+        document.querySelector('.menuHamburguer').style.filter = 'invert(0)';
+        document.querySelector('.menuContainer').style.filter = 'invert(0)';
+    }
+});
