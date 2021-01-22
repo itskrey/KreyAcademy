86444f19b4348bc0a2e302e38e2739fcc2c781e3
Krey <contact@itskrey.com>
2021-01-22 18:48:42 +0100

    Completed and responsive

--- Gaming Hero Section with header/code.js
+++ Gaming Hero Section with header/code.js
@@ -3,3 +3,13 @@ $(window).ready(function(){
     var top = teamsHeight+'px';
     document.querySelector('#ShapeLeft').style.top= top;
 });
+
+window.addEventListener("scroll", function(){
+    if(window.scrollY>=10){
+        document.querySelector('#Header').style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+        document.querySelector('#Header').style.backdropFilter = 'blur(10px)';
+    }else{
+        document.querySelector('#Header').style.backgroundColor = 'transparent';
+        document.querySelector('#Header').style.backdropFilter = 'none';
+    }
+});
