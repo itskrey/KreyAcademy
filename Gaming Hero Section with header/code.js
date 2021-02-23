a95f6d51b720d1292e4eaf27b6de23ff761ff8e7
Krey <contact@itskrey.com>
2021-02-23 13:09:14 +0100

    E-Commerce Product Card

--- Gaming Hero Section with header/code.js
+++ Gaming Hero Section with header/code.js
@@ -1,18 +1,19 @@
 $(window).ready(function(){
     var heroHeight = $('#Hero').innerHeight();
     var top = heroHeight+'px';
-    document.querySelector('#ShapeLeft').style.top= top;
+    document.querySelector('#ShapeLeft').style.top = top;
 });
 
 $(window).resize(function(){
     var heroHeight = $('#Hero').innerHeight();
     var top = heroHeight+'px';
-    document.querySelector('#ShapeLeft').style.top= top;
+    document.querySelector('#ShapeLeft').style.top = top;
 });
+
 
 window.addEventListener("scroll", function(){
     if(window.scrollY>=10){
-        document.querySelector('#Header').style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+        document.querySelector('#Header').style.backgroundColor = 'rgba(0,0,0,0.7)';
         document.querySelector('#Header').style.backdropFilter = 'blur(10px)';
     }else{
         document.querySelector('#Header').style.backgroundColor = 'transparent';
