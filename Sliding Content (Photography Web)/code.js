20dfdffa2573e58e303e7a66319cdb7a19c8959b
Krey <contact@itskrey.com>
2021-03-06 09:20:26 +0100

    Finished and Responsie

--- Sliding Content (Photography Web)/code.js
+++ Sliding Content (Photography Web)/code.js
@@ -1,17 +1,12 @@
 
-console.log("HJK");
 var numSlide = 0;
 var next = document.getElementById('Next');
 var prev = document.getElementById('Prev');
 var image = document.getElementById('Image');
 
+//Retroceder en el slider
 prev.addEventListener("click", function(){
-    if(numSlide==4){
-        document.getElementById('Slider').style.transform = 'translateX(-75%)';
-        changeImagePrev(numSlide);
-        numSlide--;
-    }
-    else if(numSlide==1){
+    if(numSlide==1){
         document.getElementById('Slider').style.transform = 'translateX(0%)';
         changeImagePrev(numSlide);
         numSlide--;
@@ -26,9 +21,14 @@ prev.addEventListener("click", function(){
         changeImagePrev(numSlide);
         numSlide--;
     }
+    else if(numSlide==4){
+        document.getElementById('Slider').style.transform = 'translateX(-75%)';
+        changeImagePrev(numSlide);
+        numSlide--;
+    }
 });
 
-
+//Avanzar en el slider
 next.addEventListener("click", function(){
     if(numSlide==0){
         document.getElementById('Slider').style.transform = 'translateX(-25%)';
@@ -45,56 +45,55 @@ next.addEventListener("click", function(){
         changeImageNext(numSlide);
         numSlide++;
     }
-    else if(numSlide==3){
-        console.log("End");
-    }
 });
+
 
 
 function changeImageNext(num){
     if(num==0){
         image.style.opacity = '0';
         setTimeout(function(){
-            image.src = 'Resources/Vietnam.jpg';
+            image.src = 'Resources/Vietnam.jpg'
             image.style.opacity = '0.5';
-        }, 500);
+        },500)
     }
     else if(num==1){
         image.style.opacity = '0';
         setTimeout(function(){
-            image.src = 'Resources/Philippines.jpg';
+            image.src = 'Resources/Philippines.jpg'
             image.style.opacity = '0.5';
-        }, 500);
+        },500)
     }
     else if(num==2){
         image.style.opacity = '0';
         setTimeout(function(){
-            image.src = 'Resources/Thailand.jpg';
+            image.src = 'Resources/Thailand.jpg'
             image.style.opacity = '0.5';
-        }, 500);
+        },500)
     }
 }
+
 
 function changeImagePrev(num){
     if(num==1){
         image.style.opacity = '0';
         setTimeout(function(){
-            image.src = 'Resources/Germany.jpg';
+            image.src = 'Resources/Germany.jpg'
             image.style.opacity = '0.5';
-        }, 500);
+        },500)
     }
     else if(num==2){
         image.style.opacity = '0';
         setTimeout(function(){
-            image.src = 'Resources/Vietnam.jpg';
+            image.src = 'Resources/Vietnam.jpg'
             image.style.opacity = '0.5';
-        }, 500);
+        },500)
     }
     else if(num==3){
         image.style.opacity = '0';
         setTimeout(function(){
-            image.src = 'Resources/Philippines.jpg';
+            image.src = 'Resources/Philippines.jpg'
             image.style.opacity = '0.5';
-        }, 500);
+        },500)
     }
 }
