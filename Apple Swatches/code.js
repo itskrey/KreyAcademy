336e7b8ba372d5cfc0a789dda9c92c67b14e96fc
Krey <contact@itskrey.com>
2021-05-31 09:45:54 +0200

    Apple Swatches

---
+++ Apple Swatches/code.js
@@ -0,0 +1,26 @@
+$('#Swatch1').click(function(){
+    document.querySelector('#Swatch1').classList.toggle('active');
+});
+$('#Swatch2').click(function(){
+    document.querySelector('#Swatch2').classList.toggle('active');
+});
+$('#Swatch3').click(function(){
+    document.querySelector('#Swatch3').classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
