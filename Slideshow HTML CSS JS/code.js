21abc9caa20bd9c044c2726b4fa88408fe1d5024
Krey <contact@itskrey.com>
2021-06-02 10:41:28 +0200

    Slideshow HTML CSS JS

---
+++ Slideshow HTML CSS JS/code.js
@@ -0,0 +1,41 @@
+var numSlide = 0;
+var next = document.getElementById('Next');
+var prev = document.getElementById('Prev');
+var image = document.getElementById('Image');
+
+//Retroceder en el slider
+prev.addEventListener("click", function(){
+    changeImagePrev(numSlide);
+    numSlide--;
+});
+
+//Avanzar en el slider
+next.addEventListener("click", function(){
+    changeImageNext(numSlide);
+    numSlide++;
+});
+
+
+var arrayImages = ["Resources/Imagen1.jpg", "Resources/Imagen2.jpg", "Resources/Imagen3.jpg"];
+
+function changeImagePrev(num){
+    image.style.opacity ='0';
+    setTimeout(function(){
+        var position = (num - 1) % arrayImages.length
+        image.src = arrayImages[position];
+    }, 500)
+    setTimeout(function(){
+        image.style.opacity = '0.5';  
+    }, 700)
+}
+function changeImageNext(num){
+    image.style.opacity ='0';
+    setTimeout(function(){
+        var position = (num + 1) % arrayImages.length
+        image.src = arrayImages[position];
+    }, 500)
+    setTimeout(function(){
+        image.style.opacity = '0.5';  
+    }, 700)
+    
+}
