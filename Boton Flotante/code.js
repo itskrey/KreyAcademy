ced9d63e927e5d325a5d5ef1df2e3b1cf112b359
Krey <contact@itskrey.com>
2021-08-06 09:00:14 +0200

    Botón Flotante

---
+++ Boton Flotante/code.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', ()=>{
+    let button = document.querySelector('.chat-button')
+
+    button.addEventListener('click', (e)=>{
+        e.preventDefault()
+
+        //Programa aquí lo que necesites
+        alert('Mostrando botón')
+    })
+})
